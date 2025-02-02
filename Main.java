32521cb28a28cb1c33a9d500f6c3059b4c4a55f9
9534495797 <chaubeyankit927@gmail.com>
2025-02-02 17:11:41 +0530

    ok

--- Main.java
+++ Main.java
@@ -1,14 +1,29 @@
-interface Anomnymous{
-    void sayhello();
-}
+// interface Anomnymous{
+//     void sayhello();
+// }
+// public class Main{
+//     public static void main(String[]args){
+// Anomnymous ab=new Anomnymous() {
+//     @Override
+//     public void sayhello(){
+//         System.out.println("hello ankit");
+//     }
+// };
+// ab.sayhello();
+//     }
+// }
+
+import java.util.Arrays;
+import java.util.List;
 public class Main{
     public static void main(String[]args){
-Anomnymous ab=new Anomnymous() {
-    @Override
-    public void sayhello(){
-        System.out.println("hello ankit");
+List<Integer>numbers=Arrays.asList(1,2,3,4,5);
+int count=0;
+for(int i:numbers){
+    if(i%2==0){
+        count++;
     }
-};
-ab.sayhello();
-    }
+}
+System.out.println(count);
+}
 }
