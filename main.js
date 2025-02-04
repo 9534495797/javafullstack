68f99900e337c65d78fed58b58d3d47d43e8c926
9534495797 <chaubeyankit927@gmail.com>
2025-02-04 09:30:41 +0530

    ok

---
+++ main.js
@@ -0,0 +1,13 @@
+// alert("hello guys")
+console.log("this is log")
+let name="Ankit";
+
+// template leiteral ES6
+const newstatement=`${name} is good 
+in js`;
+console.log(newstatement);
+let i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
