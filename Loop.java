c05b70d7e1c5ec2f8f091e1c01c24fa84cf80fa5
9534495797 <chaubeyankit927@gmail.com>
2025-02-22 19:23:57 +0530

    ok

---
+++ Loop.java
@@ -0,0 +1,12 @@
+public class Loop {
+    public static void main(String[] args) {
+        // for(int i=1;i<=5;i++){
+        //     System.out.println("*");
+        // }
+        int i=0;
+        while( i<5){
+            System.out.println("*");
+            i++;
+        }
+    }
+}
